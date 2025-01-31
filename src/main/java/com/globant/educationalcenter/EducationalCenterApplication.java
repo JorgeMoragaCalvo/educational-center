fd5924a980ecf9b3b20096987cd44b8e4409587d
JorgeMoragaCalvo <jorge.moraga.c@usach.cl>
2025-01-31 16:28:44 -0300

    first commit

---
+++ src/main/java/com/globant/educationalcenter/EducationalCenterApplication.java
@@ -0,0 +1,13 @@
+package com.globant.educationalcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationalCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducationalCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/globant/educationalcenter/EducationalCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globant.educationalcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationalCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
