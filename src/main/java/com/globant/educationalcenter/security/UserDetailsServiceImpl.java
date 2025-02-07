59087e8c8af837cdb16310249bbfcc9f29b7bc5f
JorgeMoragaCalvo <jorge.moraga.c@usach.cl>
2025-02-07 16:55:59 -0300

    UserDetailsServiceImpl implementation

---
+++ src/main/java/com/globant/educationalcenter/security/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.globant.educationalcenter.security;
+
+import com.globant.educationalcenter.repositories.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional // When user is loaded also load role and authorities (?)
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
